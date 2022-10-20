762a1bc98d605a30c8597b7fce2bc37d143ba6f3
pdv88 <888pdv@gmail.com>
2022-10-20 12:49:11 -0500

    solve all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(arg1, arg2) {
+    let fullArr = []
+    let total = 0
+    const reducer = (accumulator, currentValue) => accumulator + currentValue
+    if (typeof arg1 != "number" || typeof arg2 != "number") {
+        return "ERROR"
+    } else if (arg1 < 0 || arg2 < 0) {
+        return "ERROR"
+    }else if (arg1 > arg2) {
+        for (let i = arg2; i <= arg1; i++){
+            fullArr.push(i)
+        }
+    } else if (arg1 < arg2) {
+        for (let i = arg1; i <= arg2; i++){
+            fullArr.push(i)
+        }
+    } 
+    total = fullArr.reduce(reducer)
+    return total
 };
 
 // Do not edit below this line
