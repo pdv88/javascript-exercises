067fc19057c87c1ff4abe817cfc0950ad1293b84
pdv88 <888pdv@gmail.com>
2022-10-20 14:21:26 -0500

    solve all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  temp = (num - 32)/1.8
+  roundTemp = temp.toFixed(1)
+  return parseFloat(roundTemp)
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  temp = (num * 1.8) + 32
+  roundTemp = temp.toFixed(1)
+  return parseFloat(roundTemp)
 };
 
 // Do not edit below this line
