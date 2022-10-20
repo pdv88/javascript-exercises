976d4f099ff2cda2baecdc592526c9a9e9d1f3c5
pdv88 <888pdv@gmail.com>
2022-10-19 20:30:41 -0500

    Pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    splitStr = string.split("")
+    console.log(splitStr)
+    
+    reverseStr = splitStr.reverse()
+    console.log(reverseStr)
+    
+    joinStr = reverseStr.join("")
+    console.log(joinStr)
+    
+    return joinStr
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
