fee54c85915c08487e26acbb6026efc2b8bf8100
pdv88 <888pdv@gmail.com>
2022-10-20 13:39:25 -0500

    solve all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    checkLeap = year / 4
+    checkCentury = year / 100
+    checkSpecial = year / 400
+    if (Number.isInteger(checkLeap) == true && Number.isInteger(checkCentury) == false) {
+        return true
+    } else if (Number.isInteger(checkSpecial) == true) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
