7ed1a6dbae6ea129f4ba2325c8b6e73351c9e027
pdv88 <888pdv@gmail.com>
2022-10-25 13:11:17 -0500

    pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    for(let person of people){
+        if (person.hasOwnProperty('yearOfDeath') == false){
+            person.yearOfDeath = 2022
+        }
+    }
+    const oldest = people.sort(function(a, b) {
+        const lastPerson = a.yearOfDeath - a.yearOfBirth
+        const nextPerson = b.yearOfDeath - b.yearOfBirth
 
+        if(lastPerson > nextPerson){
+            return -1
+        } else {
+            return 1
+        }
+    })
+    return people[0]
 };
 
 // Do not edit below this line
