ff36a5778cf0917661d01549d461ac79961372bd
pdv88 <888pdv@gmail.com>
2022-10-24 19:46:21 -0500

    pass all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0
+  for (let num of arr){
+    total += num
+  }
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1
+  for (let num of arr){
+    total *= num
+  }
+  return total
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let total = 1
+  for (let i = 0; i < b; i++){
+    total = total * a
+  }
+  return total
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1
+  for(let i = num; i > 0; i--){
+    total = total * i
+  }
+  return total
 };
 
 // Do not edit below this line
@@ -31,3 +47,4 @@ module.exports = {
   power,
   factorial
 };
+
