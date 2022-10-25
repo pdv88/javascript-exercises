f93f2564d31f2be87538916b0f68a681b7042e2f
pdv88 <888pdv@gmail.com>
2022-10-24 21:02:52 -0500

    pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibNum) {
+    Number(fibNum)
+    let a = 0, b = 1, c = fibNum    
+    for (i = 2; i <= fibNum; i++){
+        c = a + b
+        a = b
+        b = c
+    }
+    if (fibNum < 0){
+        return "OOPS"
+    } else if (fibNum == 1){
+        return 1
+    }
+    return c
 };
 
 // Do not edit below this line
