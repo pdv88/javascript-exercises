ba970d8997b3b9e5b5edccb8946691a9802c8b3f
pdv88 <888pdv@gmail.com>
2022-10-24 20:21:57 -0500

    pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    lowerStr = string.toLowerCase()
+    noSpaceStr = lowerStr.replace(/\s+/g, '')
+    noSymStr = noSpaceStr.replace(/[^a-zA-Z ]/g, "") 
+    splitStr = noSymStr.split("")
+    reverseStr = splitStr.reverse()
+    joinStr = reverseStr.join("")
 
+    if(noSymStr === joinStr){
+        return true 
+    } else {
+        return false
+    }
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
